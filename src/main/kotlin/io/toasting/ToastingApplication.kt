94280956a40f00571762656a6bb34953e9557dc2
howudong <tjdvy963@naver.com>
2025-01-30 20:01:20 +0900

    chore : Swagger 이슈로 인해 스프링 부트 버전 3.4 -> 3.3.1로 변경

--- src/main/kotlin/io/toasting/ToastingApplication.kt
+++ src/main/kotlin/io/toasting/ToastingApplication.kt
@@ -1,24 +1,13 @@
 package io.toasting
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication()
 @EnableJpaAuditing
-class ToastingApplication : CommandLineRunner {
-    @Autowired
-    lateinit var ac: ApplicationContext
-
-    override fun run(vararg args: String?) {
-        ac.beanDefinitionNames.forEach { println(it) }
-    }
-}
+class ToastingApplication
 
 fun main(args: Array<String>) {
-    runApplication<ToastingApplication>(*args) {
-    }
+    runApplication<ToastingApplication>(*args)
 }
