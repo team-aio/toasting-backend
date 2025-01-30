84ebf3fdcdf929538f22cd230d2c2e6c70aad46a
howudong <tjdvy963@naver.com>
2025-01-30 11:39:46 +0900

    feat : SecurityConfig 추가로 Stateless 모드로 전환 및 여러 config 추가

---
+++ src/main/kotlin/io/toasting/global/config/SecurityConfig.kt
@@ -0,0 +1,57 @@
+package io.toasting.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun corsConfigSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+        corsConfig.apply {
+            allowedHeaders = listOf("*")
+            allowedOrigins = listOf("http://localhost:3000")
+            exposedHeaders = listOf("*")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowCredentials = true
+            maxAge = 3600
+        }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+        return source
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigSource()) }
+            .formLogin { it.disable() } // cors 설정 활성화
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        http.authorizeHttpRequests {
+            it
+                .requestMatchers(
+                    "/",
+                    "/h2-console/**",
+                    "/favicon.ico",
+                    "/error",
+                    "/swagger-ui/**",
+                    "/swagger-resources/**",
+                    "/v3/api-docs/**",
+                ).permitAll()
+                .anyRequest()
+                .authenticated()
+        }
+        return http.build()
+    }
+}
