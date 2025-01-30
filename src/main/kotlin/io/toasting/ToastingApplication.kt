e6620e78a124ee6137add92dc5a983ec9b819bbd
howudong <tjdvy963@naver.com>
2025-01-30 19:47:34 +0900

    fix : @EnableJpaAuditing 추가로 BaseEntity 정상작동화

--- src/main/kotlin/io/toasting/ToastingApplication.kt
+++ src/main/kotlin/io/toasting/ToastingApplication.kt
@@ -1,11 +1,24 @@
 package io.toasting
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@SpringBootApplication
-class ToastingApplication
+@SpringBootApplication()
+@EnableJpaAuditing
+class ToastingApplication : CommandLineRunner {
+    @Autowired
+    lateinit var ac: ApplicationContext
+
+    override fun run(vararg args: String?) {
+        ac.beanDefinitionNames.forEach { println(it) }
+    }
+}
 
 fun main(args: Array<String>) {
-    runApplication<ToastingApplication>(*args)
+    runApplication<ToastingApplication>(*args) {
+    }
 }
