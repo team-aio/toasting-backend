37740159efd4b78789da62304e1b988c31147292
howudong <tjdvy963@naver.com>
2025-01-30 19:47:58 +0900

    fix : noArgs 추가로 기본생성자를 생성하게함

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,6 +60,12 @@ allOpen {
     annotation("jakarta.persistence.Embeddable")
 }
 
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
