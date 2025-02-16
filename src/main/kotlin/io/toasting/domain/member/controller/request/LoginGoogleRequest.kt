24de58acd7325809bd84434e5f50039862837564
howudong <tjdvy963@naver.com>
2025-02-16 18:42:39 +0900

    feat : 구글 소셜 로그인 요청 값에 username 추가

--- src/main/kotlin/io/toasting/domain/member/controller/request/LoginGoogleRequest.kt
+++ src/main/kotlin/io/toasting/domain/member/controller/request/LoginGoogleRequest.kt
@@ -2,13 +2,17 @@ package io.toasting.domain.member.controller.request
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
-@Schema(description = "구글 로그인 요청")
+@Schema(title = "구글 로그인 요청 DTO", description = "구글 로그인 요청")
 data class LoginGoogleRequest(
     @Schema(description = "이메일", example = "howudong@example.com")
     @field:Email(message = "이메일 형식이 아닙니다.")
     val email: String,
+    @Schema(description = "유저 이름", example = "howudong")
+    @field:NotBlank(message = "유저 이름은 빈 값이 될 수 없습니다.")
+    val username: String,
     @Schema(description = "snsType", example = "google")
     @NotNull(message = "snsType은 필수입니다.")
     val snsType: String,
