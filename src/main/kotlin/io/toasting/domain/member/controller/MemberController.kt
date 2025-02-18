d0aea6761daa469049adcca71be0632f833baddb
howudong <tjdvy963@naver.com>
2025-02-18 22:48:54 +0900

    feat : 기존의 responseBody로 응답이 가던 것을 accessToken으 response header로, refreshToken은 cookie로 가도록 변경

--- src/main/kotlin/io/toasting/domain/member/controller/MemberController.kt
+++ src/main/kotlin/io/toasting/domain/member/controller/MemberController.kt
@@ -2,15 +2,15 @@ package io.toasting.domain.member.controller
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.toasting.api.code.status.SuccessStatus
 import io.toasting.domain.member.application.LoginMemberService
 import io.toasting.domain.member.controller.request.LoginGoogleRequest
-import io.toasting.domain.member.controller.response.LoginGoogleResponse
 import io.toasting.global.api.ApiResponse
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/member")
 @Tag(name = "Member", description = "회원 관련 API")
 class MemberController(
+    @Value("\${spring.jwt.refresh-token-expired-ms}") private val refreshTokenExpiredMs: Long,
     private val loginMemberService: LoginMemberService,
 ) {
     private val log = KotlinLogging.logger {}
@@ -29,12 +30,6 @@ class MemberController(
     @io.swagger.v3.oas.annotations.responses.ApiResponse(
         responseCode = "200",
         description = "로그인 성공, 기존에 가입했던 유저",
-        content = [
-            Content(
-                schema = Schema(implementation = LoginGoogleResponse::class),
-                mediaType = "application/json",
-            ),
-        ],
     )
     @io.swagger.v3.oas.annotations.responses.ApiResponse(
         responseCode = "201",
@@ -42,13 +37,27 @@ class MemberController(
     )
     fun loginGoogle(
         @Valid @RequestBody loginGoogleRequest: LoginGoogleRequest,
-    ): ApiResponse<LoginGoogleResponse?> {
+        response: HttpServletResponse,
+    ): ApiResponse<Unit> {
         val loginGoogleOutput = processGoogleLogin(loginGoogleRequest)
 
-        return loginGoogleOutput
-            ?.let { ApiResponse.onSuccess(LoginGoogleResponse.from(loginGoogleOutput)) }
-            ?: run { ApiResponse.onSuccess(SuccessStatus.MEMBER_CREATED.status, null) }
+        if (loginGoogleOutput != null) {
+            response.setHeader("Authorization", loginGoogleOutput.accessToken)
+            response.addCookie(createCookie("RefreshToken", loginGoogleOutput.refreshToken))
+            return ApiResponse.onSuccess()
+        }
+        return ApiResponse.onSuccess(SuccessStatus.MEMBER_CREATED.status, null)
     }
+
+    private fun createCookie(
+        key: String,
+        value: String,
+    ): Cookie =
+        Cookie(key, value).apply {
+            isHttpOnly = true
+            maxAge = (refreshTokenExpiredMs / 1000).toInt()
+            path = "/"
+        }
 
     private fun processGoogleLogin(loginGoogleRequest: LoginGoogleRequest) =
         loginGoogleRequest
