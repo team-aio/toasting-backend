1bf89ae65d8a45648eb26eeead5e7f302168b877
preferrrr <zzangsh2946@naver.com>
2025-03-03 00:59:12 +0900

    feat : 메세지 보내기 service 테스트 코드 추가

--- src/test/kotlin/io/toasting/domain/message/application/MessageServiceTest.kt
+++ src/test/kotlin/io/toasting/domain/message/application/MessageServiceTest.kt
@@ -3,10 +3,15 @@ package io.toasting.domain.message.application
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.toasting.creator.MessageCreator
+import io.toasting.domain.member.entity.Member
 import io.toasting.domain.member.entity.MemberDetails
+import io.toasting.domain.member.repository.MemberRepository
 import io.toasting.domain.member.vo.RoleType
 import io.toasting.domain.message.applicatoin.MessageService
+import io.toasting.domain.message.applicatoin.`in`.SendMessageInput
+import io.toasting.domain.message.applicatoin.out.SendMessageOutput
 import io.toasting.domain.message.entity.Message
 import io.toasting.domain.message.repository.MessageRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,12 +24,18 @@ class MessageServiceTest private constructor() : BehaviorSpec() {
     override fun extensions() = listOf(SpringExtension)
 
     @Autowired
+    private lateinit var memberRepository: MemberRepository
+    @Autowired
     private lateinit var messageRepository: MessageRepository
-
     @Autowired
     private lateinit var messageService: MessageService
 
     init {
+        afterTest {
+            messageRepository.deleteAll()
+            memberRepository.deleteAll()
+        }
+
         Given("member가 읽지 않은 메세지가 10개라면,") {
             val memberDetails = MemberDetails(RoleType.ROLE_USER.name, 1)
             val messageList: MutableList<Message> = mutableListOf()
@@ -41,6 +52,31 @@ class MessageServiceTest private constructor() : BehaviorSpec() {
 
                 Then("10이 반환되어야 한다.") {
                     result.count shouldBe 10
+                }
+            }
+        }
+
+        Given("sender와 receiver가 주어지고,") {
+            val sender = Member.defaultMember("sender", "sender@test.com")
+            val receiver = Member.defaultMember("receiver", "receiver@test.com")
+            memberRepository.saveAll(listOf(sender, receiver))
+
+            val memberDetail = MemberDetails.from(sender)
+            val input = SendMessageInput(2, 3, "test")
+
+            When("sender가 receiver에게 'test'라는 메세지를 보내면,") {
+                val result: SendMessageOutput = messageService.sendMessage(memberDetail, input)
+
+                Then("메세지가 저장되고, 응답으로 메세지 정보가 반환된다.") {
+
+                    val messageList: MutableList<Message> = messageRepository.findAll();
+                    messageList.size shouldBe 1
+
+                    result.id shouldNotBe null
+                    result.receiverId shouldBe 2
+                    result.postId shouldBe 3
+                    result.content shouldBe "test"
+                    result.createdAt shouldNotBe null
                 }
             }
         }
