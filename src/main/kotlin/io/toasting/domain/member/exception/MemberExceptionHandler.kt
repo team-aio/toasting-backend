973ce96d99626928d008239460f4488e19ff9de4
howudong <tjdvy963@naver.com>
2025-03-03 10:49:38 +0900

    refactor : Google 한정으로 되어있던 이름을 모든 소셜 로그인에서 공용 이용 가능하도록 리팩토링

---
+++ src/main/kotlin/io/toasting/domain/member/exception/MemberExceptionHandler.kt
@@ -0,0 +1,10 @@
+package io.toasting.domain.member.exception
+
+import io.toasting.api.code.BaseErrorCode
+import io.toasting.global.api.exception.GeneralException
+
+sealed class MemberExceptionHandler {
+    class MemberNameDuplicationException(
+        errorCode: BaseErrorCode,
+    ) : GeneralException(errorCode)
+}
