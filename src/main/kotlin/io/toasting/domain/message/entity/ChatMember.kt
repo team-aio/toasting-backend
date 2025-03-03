ef8a7d415b618a470acd131df272986f11f43907
preferrrr <zzangsh2946@naver.com>
2025-03-03 16:09:44 +0900

    feat : ChatMember, Message에 ChatRoom 연관관계 추가

--- src/main/kotlin/io/toasting/domain/message/entity/ChatMember.kt
+++ src/main/kotlin/io/toasting/domain/message/entity/ChatMember.kt
@@ -1,15 +1,15 @@
 package io.toasting.domain.message.entity
 
 import io.toasting.domain.model.BaseEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 class ChatMember(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    val chatRoom: ChatRoom,
     val memberId: Long,
 ) : BaseEntity() {
 }
--- src/main/kotlin/io/toasting/domain/message/entity/Message.kt
+++ src/main/kotlin/io/toasting/domain/message/entity/Message.kt
@@ -1,17 +1,16 @@
 package io.toasting.domain.message.entity
 
 import io.toasting.domain.model.BaseEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 class Message(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    val chatRoomId: Long,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    val chatRoom: ChatRoom,
     val content: String,
     var isRead: Boolean,
 ) : BaseEntity() {
