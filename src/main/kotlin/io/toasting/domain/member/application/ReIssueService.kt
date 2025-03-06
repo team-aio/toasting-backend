79ccbbccdf21288000475484ac9909cdae5833f5
howudong <tjdvy963@naver.com>
2025-03-07 00:23:37 +0900

    feat : 액세스 토큰 재발급시 리프레시 토큰도 재발급하도록 구현

--- src/main/kotlin/io/toasting/domain/member/application/ReIssueService.kt
+++ src/main/kotlin/io/toasting/domain/member/application/ReIssueService.kt
@@ -3,27 +3,56 @@ package io.toasting.domain.member.application
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.toasting.api.code.status.ErrorStatus
 import io.toasting.domain.member.application.output.ReIssueOutput
+import io.toasting.domain.member.entity.RefreshToken
+import io.toasting.domain.member.repository.RefreshTokenRepository
 import io.toasting.global.api.exception.handler.AuthExceptionHandler
 import io.toasting.global.security.jwt.JwtFactory
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.Date
 
 @Service
 class ReIssueService(
     private val jwtFactory: JwtFactory,
+    private val refreshTokenRepository: RefreshTokenRepository,
 ) {
     private val log = KotlinLogging.logger {}
 
     fun reIssueAccessToken(refreshToken: String): ReIssueOutput {
         var output: ReIssueOutput? = null
+        // TODO : 밑에 validateRefreshToken 역할이 있는데, 여기서 따로 validate하는게 맞는건지? 추후 리팩토링 해야함
+        if (!refreshTokenRepository.existsByToken(refreshToken)) {
+            throw AuthExceptionHandler.TokenNotFoundException(ErrorStatus.REFRESH_TOKEN_NOT_FOUND)
+        }
 
         jwtFactory
             .validateRefreshToken(refreshToken)
             .onSuccess {
                 val (newAccessToken, newRefreshToken) = createNewAccessAndRefreshToken(refreshToken)
+                replaceRefreshToken(refreshToken, newRefreshToken)
                 output = ReIssueOutput(newAccessToken, newRefreshToken)
             }.onFailure { exception -> findException(exception) }
 
         return output ?: throw IllegalStateException("Unexpected error occurred")
+    }
+
+    private fun replaceRefreshToken(
+        oldRefreshToken: String,
+        newRefreshToken: String,
+    ) {
+        refreshTokenRepository.deleteByToken(oldRefreshToken)
+        val memberId = jwtFactory.memberId(newRefreshToken) ?: throw IllegalArgumentException("memberId is null")
+        val date = Date(System.currentTimeMillis() + jwtFactory.refreshExpiredMs())
+
+        val refreshToken =
+            RefreshToken(
+                memberId = memberId.toLong(),
+                token = newRefreshToken,
+                expiredAt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()),
+            )
+
+        refreshTokenRepository.save(refreshToken)
     }
 
     private fun findException(exception: Throwable) {
