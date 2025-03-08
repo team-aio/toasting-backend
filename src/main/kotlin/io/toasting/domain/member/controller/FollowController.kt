77147995e84298703d31acea7e82a4ef019a78d0
howudong <tjdvy963@naver.com>
2025-03-09 02:23:39 +0900

    feat : 팔로잉 취소 API 컨트롤러단 로직 작성

--- src/main/kotlin/io/toasting/domain/member/controller/FollowController.kt
+++ src/main/kotlin/io/toasting/domain/member/controller/FollowController.kt
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.toasting.domain.member.application.FollowService
 import io.toasting.domain.member.application.input.AddFollowInput
+import io.toasting.domain.member.application.input.CancelFollowInput
 import io.toasting.domain.member.controller.response.ExistsFollowingResponse
 import io.toasting.domain.member.entity.MemberDetails
 import io.toasting.global.api.ApiResponse
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -35,6 +37,19 @@ class FollowController(
         val addFollowInput = AddFollowInput(fromMemberId = fromMemberId, toMemberId = memberId)
 
         followService.addFollow(addFollowInput)
+        return ApiResponse.onSuccess()
+    }
+
+    @DeleteMapping("/{memberId}")
+    @Operation(summary = "팔로우 취소", description = "해당 사용자를 팔로우를 취소합니다.")
+    fun cancelFollow(
+        @PathVariable memberId: Long,
+        @AuthenticationPrincipal memberDetails: MemberDetails,
+    ): ApiResponse<Unit> {
+        val fromMemberId = memberDetails.username.toLong()
+        val cancelFollowInput = CancelFollowInput(fromMemberId = fromMemberId, toMemberId = memberId)
+
+        followService.cancelFollow(cancelFollowInput)
         return ApiResponse.onSuccess()
     }
 }
