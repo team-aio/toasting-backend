1c0cbf358714f17d8b35abfbabf707f75700c22c
howudong <tjdvy963@naver.com>
2025-03-09 13:41:21 +0900

    fix : follow가 삭제될때 member가 삭제되는 비정상적인 상황이 발생하므로 cascading 삭제

--- src/main/kotlin/io/toasting/domain/member/entity/Follow.kt
+++ src/main/kotlin/io/toasting/domain/member/entity/Follow.kt
@@ -1,7 +1,7 @@
 package io.toasting.domain.member.entity
 
-import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -10,10 +10,10 @@ import jakarta.persistence.ManyToOne
 
 @Entity
 class Follow(
-    @ManyToOne(cascade = [CascadeType.ALL])
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "from_member_id")
     val fromMember: Member,
-    @ManyToOne(cascade = [CascadeType.ALL])
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "to_member_id")
     val toMember: Member,
     @Id
