fe5c17427c0704091b2c6bba71ddd16c4ac57178
howudong <tjdvy963@naver.com>
2025-03-24 23:33:48 +0900

    !HOTFIX : 오타 제거

--- src/main/kotlin/io/toasting/global/config/SecurityConfig.kt
+++ src/main/kotlin/io/toasting/global/config/SecurityConfig.kt
@@ -24,7 +24,7 @@ class SecurityConfig {
             allowedHeaders = listOf("*")
             allowedOrigins =
                 listOf(
-                    "http://locahost",
+                    "http://localhost",
                     "https://localhost",
                     "https://toasting.io",
                     "http://toasting.io",
