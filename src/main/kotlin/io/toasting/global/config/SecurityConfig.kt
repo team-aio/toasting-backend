0f578f0d070e5afdefbdbb32676febbd290637b7
howudong <tjdvy963@naver.com>
2025-03-24 23:24:11 +0900

    !HOTFIX: LocalHost 3000번 포트 제거

--- src/main/kotlin/io/toasting/global/config/SecurityConfig.kt
+++ src/main/kotlin/io/toasting/global/config/SecurityConfig.kt
@@ -24,7 +24,8 @@ class SecurityConfig {
             allowedHeaders = listOf("*")
             allowedOrigins =
                 listOf(
-                    "http://localhost:3000",
+                    "http://locahost",
+                    "https://localhost",
                     "https://toasting.io",
                     "http://toasting.io",
                     "http://api.toasting.io",
