29dbdfbb079495606d90789a7789af792c96c3ff
howudong <tjdvy963@naver.com>
2025-04-08 22:02:05 +0900

    fix : 글자 초과 에러를 해결하기 위해 512자로 제한을 늘림

--- src/main/kotlin/io/toasting/domain/member/entity/RefreshToken.kt
+++ src/main/kotlin/io/toasting/domain/member/entity/RefreshToken.kt
@@ -1,5 +1,6 @@
 package io.toasting.domain.member.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -14,6 +15,7 @@ class RefreshToken(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private val id: Long? = null,
     private val memberId: Long,
+    @Column(length = 512)
     private val token: String? = null,
     private val expiredAt: LocalDateTime,
 )
