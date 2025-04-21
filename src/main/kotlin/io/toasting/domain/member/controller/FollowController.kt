b69e3a105aef2cda28902be0460712270dbf9747
howudong <tjdvy963@naver.com>
2025-04-21 22:58:38 +0900

    Refactor : Follow 컨트롤러에 memberId를 UUID로 바꿈

--- src/main/kotlin/io/toasting/domain/member/controller/FollowController.kt
+++ src/main/kotlin/io/toasting/domain/member/controller/FollowController.kt
@@ -3,6 +3,7 @@ package io.toasting.domain.member.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.toasting.domain.member.application.FollowService
+import io.toasting.domain.member.application.converter.MemberUuidConverter
 import io.toasting.domain.member.application.input.AddFollowInput
 import io.toasting.domain.member.application.input.CancelFollowInput
 import io.toasting.domain.member.application.input.ExistsFollowInput
@@ -16,21 +17,25 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/v1/follow")
 @Tag(name = "Following", description = "팔로우 관련 API")
 class FollowController(
     private val followService: FollowService,
+    private val memberUuidConverter: MemberUuidConverter
 ) {
     @PostMapping("/{memberId}")
     @Operation(summary = "팔로우 추가", description = "해당 사용자를 팔로우합니다.")
     fun addFollow(
-        @PathVariable memberId: Long,
+        @PathVariable memberId: String,
         @AuthenticationPrincipal memberDetails: MemberDetails,
     ): ApiResponse<Unit> {
-        val fromMemberId = memberDetails.username.toLong()
-        val addFollowInput = AddFollowInput(fromMemberId = fromMemberId, toMemberId = memberId)
+        val fromMemberId = memberUuidConverter.toMemberId(UUID.fromString(memberDetails.username))
+        val toMemberId = memberUuidConverter.toMemberId(UUID.fromString(memberId))
+
+        val addFollowInput = AddFollowInput(fromMemberId = fromMemberId, toMemberId = toMemberId)
 
         followService.addFollow(addFollowInput)
         return ApiResponse.onSuccess()
@@ -39,11 +44,13 @@ class FollowController(
     @DeleteMapping("/{memberId}")
     @Operation(summary = "팔로우 취소", description = "해당 사용자를 팔로우를 취소합니다.")
     fun cancelFollow(
-        @PathVariable memberId: Long,
+        @PathVariable memberId: String,
         @AuthenticationPrincipal memberDetails: MemberDetails,
     ): ApiResponse<Unit> {
-        val fromMemberId = memberDetails.username.toLong()
-        val cancelFollowInput = CancelFollowInput(fromMemberId = fromMemberId, toMemberId = memberId)
+        val fromMemberId = memberUuidConverter.toMemberId(UUID.fromString(memberDetails.username))
+        val toMemberId = memberUuidConverter.toMemberId(UUID.fromString(memberId))
+
+        val cancelFollowInput = CancelFollowInput(fromMemberId = fromMemberId, toMemberId = toMemberId)
 
         followService.cancelFollow(cancelFollowInput)
         return ApiResponse.onSuccess()
@@ -52,11 +59,13 @@ class FollowController(
     @GetMapping("/{memberId}/exist")
     @Operation(summary = "팔로우 여부 확인", description = "해당 사용자를 팔로우 했는지 확인합니다. 이미 했다면 true를 반환합니다.")
     fun isExistFollow(
-        @PathVariable memberId: Long,
+        @PathVariable memberId: String,
         @AuthenticationPrincipal memberDetails: MemberDetails,
     ): ApiResponse<ExistsFollowResponse> {
-        val fromMemberId = memberDetails.username.toLong()
-        val existsFollowInput = ExistsFollowInput(fromMemberId = fromMemberId, toMemberId = memberId)
+        val fromMemberId = memberUuidConverter.toMemberId(UUID.fromString(memberDetails.username))
+        val toMemberId = memberUuidConverter.toMemberId(UUID.fromString(memberId))
+        
+        val existsFollowInput = ExistsFollowInput(fromMemberId = fromMemberId, toMemberId = toMemberId)
 
         return followService
             .existsFollow(existsFollowInput)
