21dd15960fc7040fa4c9b5336b746d687ab135ac
howudong <tjdvy963@naver.com>
2025-05-17 21:02:13 +0900

    Feat : 유저 경력 조회 API 컨트롤러단 생성

--- src/main/kotlin/io/toasting/domain/company/controller/CompanyExperienceController.kt
+++ src/main/kotlin/io/toasting/domain/company/controller/CompanyExperienceController.kt
@@ -10,6 +10,7 @@ import io.toasting.domain.member.exception.MemberExceptionHandler.MemberExceptio
 import io.toasting.global.api.ApiResponse
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -21,6 +22,15 @@ import org.springframework.web.bind.annotation.RequestMapping
 class CompanyExperienceController(
     private val addCompanyExperienceService: AddCompanyExperienceService
 ) {
+
+    // TODO : 로그인을 안해도 해당 API를 호출할 수 있도록 JWT Filter를 뚫어놔야 함(현재 안뚫려있음)
+    @GetMapping("{memberId}/experience/company")
+    @Operation(summary = "유저 회사 경력 조회", description = "회사 경력을 조회합니다. 로그인이 필요 없습니다.")
+    fun getCompanyExperience(
+        @PathVariable memberId: String,
+    ): ApiResponse<Unit> {
+        return ApiResponse.onSuccess()
+    }
 
     @PostMapping("{memberId}/experience/company")
     @Operation(summary = "유저 회사 경력 추가", description = "회사 경력을 추가합니다.")
