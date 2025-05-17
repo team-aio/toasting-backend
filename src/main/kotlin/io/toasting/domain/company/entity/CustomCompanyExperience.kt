a93e1b5fb87bb7da14eae4e9018df7173157a7fd
howudong <tjdvy963@naver.com>
2025-05-17 15:59:22 +0900

    Feat : CustomCompanyExperience 엔티티 추가

---
+++ src/main/kotlin/io/toasting/domain/company/entity/CustomCompanyExperience.kt
@@ -0,0 +1,34 @@
+package io.toasting.domain.company.entity
+
+import io.toasting.domain.model.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDate
+
+@Entity
+@Table(name = "custom_company_experience")
+class CustomCompanyExperience(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column("start_date")
+    val startDate: LocalDate,
+    @Column("end_date")
+    val endDate: LocalDate,
+    @Column("position")
+    val position: String,
+    @Column("activities", columnDefinition = "text")
+    val activities: String,
+    @Column("profile_image")
+    val profileImage: String,
+    @Column("company_name")
+    val companyName: String,
+    @Column("member_id")
+    val memberId: Long,
+    @Column("is_view")
+    val isView: Boolean,
+) : BaseEntity()
