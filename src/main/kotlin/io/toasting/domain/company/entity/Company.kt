11b3d89d2b657e6c0c89b23f59e2a1bdb52e12c1
howudong <tjdvy963@naver.com>
2025-05-17 15:46:36 +0900

    Feat : Company 엔티티에 BaseEntity 추가

--- src/main/kotlin/io/toasting/domain/company/entity/Company.kt
+++ src/main/kotlin/io/toasting/domain/company/entity/Company.kt
@@ -1,5 +1,6 @@
 package io.toasting.domain.company.entity
 
+import io.toasting.domain.model.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
@@ -16,4 +17,4 @@ class Company(
 
     @Column(name = "name")
     val name: String
-)
+) : BaseEntity()
