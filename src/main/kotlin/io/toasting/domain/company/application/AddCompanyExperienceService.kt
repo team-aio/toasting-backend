65ba6fb8df4733d819c6c021a24779b4ca807504
howudong <tjdvy963@naver.com>
2025-05-17 17:55:04 +0900

    Feat : Service 로직으로 분기 TODO 완성

---
+++ src/main/kotlin/io/toasting/domain/company/application/AddCompanyExperienceService.kt
@@ -0,0 +1,21 @@
+package io.toasting.domain.company.application
+
+import org.springframework.stereotype.Service
+
+@Service
+class AddCompanyExperienceService {
+
+    /*
+     * 이미 존재하는 회사로 경력을 추가하는 경우
+     */
+    fun addExistCompanyExperience() {
+        //TODO
+    }
+
+    /*
+     * 직접 입력으로 경력을 추가하는 경우
+     */
+    fun addCustomCompanyExperience() {
+        //TODO
+    }
+}
--- src/main/kotlin/io/toasting/domain/company/controller/CompanyExperienceController.kt
+++ src/main/kotlin/io/toasting/domain/company/controller/CompanyExperienceController.kt
@@ -3,9 +3,11 @@ package io.toasting.domain.company.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.toasting.api.code.status.ErrorStatus
+import io.toasting.domain.company.application.AddCompanyExperienceService
 import io.toasting.domain.company.controller.request.AddCompanyExperienceRequest
 import io.toasting.domain.member.entity.MemberDetails
 import io.toasting.domain.member.exception.MemberExceptionHandler.MemberException
+import io.toasting.global.api.ApiResponse
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 @RequestMapping("/v1/members")
 @Tag(name = "Member", description = "회원 관련 API")
-class CompanyExperienceController {
+class CompanyExperienceController(
+    private val addCompanyExperienceService: AddCompanyExperienceService
+) {
 
     @PostMapping("{memberId}/experience/company")
     @Operation(summary = "유저 회사 경력 추가", description = "회사 경력을 추가합니다.")
@@ -24,10 +28,16 @@ class CompanyExperienceController {
         @PathVariable memberId: String,
         @AuthenticationPrincipal memberDetails: MemberDetails,
         @RequestBody request: AddCompanyExperienceRequest,
-    ) {
+    ): ApiResponse<Unit> {
         val memberUuid = memberDetails.username
         if (memberId != memberUuid) {
             throw MemberException(ErrorStatus.MEMBER_NOT_MINE)
         }
+
+        when (request.isCustom) {
+            true -> addCompanyExperienceService.addCustomCompanyExperience()
+            false -> addCompanyExperienceService.addExistCompanyExperience()
+        }
+        return ApiResponse.onSuccess()
     }
 }
