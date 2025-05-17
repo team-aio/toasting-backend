96e990795631f66e868d8a2ecd9a60729384d049
howudong <tjdvy963@naver.com>
2025-05-17 15:45:55 +0900

    Feat : Company 엔티티 추가

---
+++ src/main/kotlin/io/toasting/domain/company/entity/Company.kt
@@ -0,0 +1,19 @@
+package io.toasting.domain.company.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "company")
+class Company(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "name")
+    val name: String
+)
