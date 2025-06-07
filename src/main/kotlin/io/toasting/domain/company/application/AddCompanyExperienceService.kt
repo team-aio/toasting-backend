31d3a2cbe41d233c31ca55326a6edc89a8a4b250
howudong <tjdvy963@naver.com>
2025-06-07 17:36:13 +0900

    Refactor : 불필요한 검증 코드 삭제

--- src/main/kotlin/io/toasting/domain/company/application/AddCompanyExperienceService.kt
+++ src/main/kotlin/io/toasting/domain/company/application/AddCompanyExperienceService.kt
@@ -1,13 +1,10 @@
 package io.toasting.domain.company.application
 
-import io.toasting.api.code.status.ErrorStatus
 import io.toasting.domain.company.application.input.AddCustomCompanyExperienceInput
 import io.toasting.domain.company.application.input.AddExistCompanyExperienceInput
 import io.toasting.domain.company.repository.CompanyExperienceRepository
 import io.toasting.domain.company.repository.CompanyRepository
 import io.toasting.domain.company.repository.CustomCompanyExperienceRepository
-import io.toasting.domain.company.exception.CompanyExceptionHandler.CompanyNotFoundException
-import io.toasting.domain.member.exception.MemberExceptionHandler.MemberNotFoundException
 import io.toasting.domain.member.repository.MemberRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -21,19 +18,12 @@ class AddCompanyExperienceService(
 ) {
     @Transactional(readOnly = false)
     fun addExistCompanyExperience(input: AddExistCompanyExperienceInput) {
-        memberRepository.findById(input.memberId)
-            .orElseThrow { MemberNotFoundException(ErrorStatus.MEMBER_NOT_FOUND) }
-        companyRepository.findById(input.companyId)
-            .orElseThrow { CompanyNotFoundException(ErrorStatus.COMPANY_NOT_FOUND) }
-
         val companyExperience = input.toEntity()
         companyExperienceRepository.save(companyExperience)
     }
 
     @Transactional(readOnly = false)
     fun addCustomCompanyExperience(input: AddCustomCompanyExperienceInput) {
-        memberRepository.findById(input.memberId)
-            .orElseThrow { MemberNotFoundException(ErrorStatus.MEMBER_NOT_FOUND) }
 
         val customCompanyExperience = input.toEntity()
         customCompanyExperienceRepository.save(customCompanyExperience)
