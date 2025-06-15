a684bdc28b06fb1b7d51f4eb38f110e446a10c1e
howudong <tjdvy963@naver.com>
2025-06-15 16:17:52 +0900

    HOTFIX : 잘못된 테스트 수정으로 깨지는 빌드 수정

--- src/test/kotlin/io/toasting/domain/company/application/AddCompanyExperienceServiceTest.kt
+++ src/test/kotlin/io/toasting/domain/company/application/AddCompanyExperienceServiceTest.kt
@@ -1,20 +1,16 @@
 package io.toasting.domain.company.application
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.extensions.spring.SpringExtension
 import io.kotest.extensions.spring.SpringTestExtension
 import io.kotest.extensions.spring.SpringTestLifecycleMode
 import io.kotest.matchers.shouldBe
 import io.toasting.domain.company.application.input.AddCustomCompanyExperienceInput
 import io.toasting.domain.company.application.input.AddExistCompanyExperienceInput
 import io.toasting.domain.company.entity.Company
-import io.toasting.domain.company.exception.CompanyExceptionHandler
 import io.toasting.domain.company.repository.CompanyExperienceRepository
 import io.toasting.domain.company.repository.CompanyRepository
 import io.toasting.domain.company.repository.CustomCompanyExperienceRepository
 import io.toasting.domain.member.entity.Member
-import io.toasting.domain.member.exception.MemberExceptionHandler
 import io.toasting.domain.member.repository.MemberRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -86,44 +82,6 @@ class AddCompanyExperienceServiceTest : BehaviorSpec() {
             }
         }
 
-        Given("존재하지 않는 멤버 ID로 회사 경험을 추가하려고 할 때") {
-            val badInput = AddExistCompanyExperienceInput(
-                memberId = -1L,
-                companyId = company.id!!,
-                startDate = LocalDate.of(2021, 1, 1),
-                endDate = LocalDate.of(2023, 1, 1),
-                position = "Backend",
-                activities = "백엔드 개발 참여",
-            )
-
-            When("addExistCompanyExperience를 호출하면") {
-                Then("MemberNotFoundException이 발생한다") {
-                    shouldThrow<MemberExceptionHandler.MemberNotFoundException> {
-                        addCompanyExperienceService.addExistCompanyExperience(badInput)
-                    }
-                }
-            }
-        }
-
-        Given("존재하지 않는 회사 ID로 회사 경험을 추가하려고 할 때") {
-            val badCompanyInput = AddExistCompanyExperienceInput(
-                memberId = member.id!!,
-                companyId = -1L,
-                startDate = LocalDate.of(2021, 1, 1),
-                endDate = LocalDate.of(2023, 1, 1),
-                position = "Backend",
-                activities = "백엔드 개발 참여",
-            )
-
-            When("addExistCompanyExperience를 호출하면") {
-                Then("CompanyNotFoundException이 발생한다") {
-                    shouldThrow<CompanyExceptionHandler.CompanyNotFoundException> {
-                        addCompanyExperienceService.addExistCompanyExperience(badCompanyInput)
-                    }
-                }
-            }
-        }
-
         Given("직접 입력 회사 경험 추가 입력이 주어질 때") {
             val input = AddCustomCompanyExperienceInput(
                 memberId = member.id!!,
@@ -146,26 +104,6 @@ class AddCompanyExperienceServiceTest : BehaviorSpec() {
                     exp.companyName shouldBe "사이드프로젝트팀"
                     exp.position shouldBe "Fullstack"
                     exp.isView shouldBe true
-                }
-            }
-        }
-
-        Given("존재하지 않는 멤버 ID로 커스텀 회사 경험을 추가하려고 할 때") {
-            val badCustomInput = AddCustomCompanyExperienceInput(
-                memberId = -1L,
-                name = "사이드프로젝트팀",
-                startDate = LocalDate.of(2020, 1, 1),
-                endDate = null,
-                position = "Fullstack",
-                activities = "개발 및 운영",
-                imageUrl = "custom.png",
-            )
-
-            When("addCustomCompanyExperience를 호출하면") {
-                Then("MemberNotFoundException이 발생한다") {
-                    shouldThrow<MemberExceptionHandler.MemberNotFoundException> {
-                        addCompanyExperienceService.addCustomCompanyExperience(badCustomInput)
-                    }
                 }
             }
         }
