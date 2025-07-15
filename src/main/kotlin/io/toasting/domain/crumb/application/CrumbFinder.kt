a94252192b4d33ed8642d722d0b175c1d92ab97f
howudong <tjdvy963@naver.com>
2025-07-16 00:40:23 +0900

    Feat : CrumFinder에 대한 인터페이스 정의

---
+++ src/main/kotlin/io/toasting/domain/crumb/application/CrumbFinder.kt
@@ -0,0 +1,13 @@
+package io.toasting.domain.crumb.application
+
+import io.toasting.domain.crumb.entity.Crumb
+import java.time.LocalDate
+
+/**
+ * 빵가루 기록 조회에 관한 인터페이스
+ */
+interface CrumbFinder {
+    fun findBy(memberId: Long, activityDate: LocalDate): Crumb?
+
+    fun findAllBy(memberId: Long, startDate: LocalDate, endDate: LocalDate): List<Crumb>
+}
