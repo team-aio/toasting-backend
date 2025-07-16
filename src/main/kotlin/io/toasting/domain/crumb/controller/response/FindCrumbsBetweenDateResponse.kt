b488024bced0c0aa0c8abcac5b18c4e47b7187c4
howudong <tjdvy963@naver.com>
2025-07-17 01:20:12 +0900

    Refactor : FindCrumbsBetweenDateResponse에서 불필요한 필드 제거

--- src/main/kotlin/io/toasting/domain/crumb/controller/response/FindCrumbsBetweenDateResponse.kt
+++ src/main/kotlin/io/toasting/domain/crumb/controller/response/FindCrumbsBetweenDateResponse.kt
@@ -5,20 +5,12 @@ import io.toasting.domain.crumb.application.output.FindCrumbOutput
 data class FindCrumbsBetweenDateResponse(
     val activityCount: Int,
     val activityDate: String,
-    val commentIds: List<Long>,
-    val postIds: List<Long>,
-    val bookmarkIds: List<Long>,
-    val likeIds: List<Long>,
 ) {
     companion object {
         fun from(output: FindCrumbOutput): FindCrumbsBetweenDateResponse {
             return FindCrumbsBetweenDateResponse(
                 activityCount = output.activityCount,
                 activityDate = output.activityDate.toString(),
-                commentIds = output.commentIds,
-                postIds = output.postIds,
-                bookmarkIds = output.bookmarkIds,
-                likeIds = output.likeIds,
             )
         }
     }
